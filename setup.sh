bd0b576c88ac7c1807d85fbe823c5530f4dc6786
Dayal Mukati <dayalmukati@gmail.com>
2026-07-19 17:54:50 +0530

    Revert "setup.sh: remove cloned .git so the challenge IDE can initialize its own repo"
    This reverts commit 51a27d824a2d7ed2b3d5898059bc78362056eb75.

--- setup.sh
+++ setup.sh
@@ -14,10 +14,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Remove the clone's git metadata. The challenge IDE initializes its own git
-# repository in this directory; leaving our .git behind makes its "git init" hit
-# an existing repo and its "git remote add origin" fail with
-# "remote origin already exists".
-rm -rf "$TARGET_DIR/.git"
-
 echo "Setup complete. Files downloaded."
